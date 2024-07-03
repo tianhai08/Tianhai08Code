0ec107f9f2e678d0cae254926759d02b5d3ca2ec
tianhai08 <166911681+tianhai08@users.noreply.github.com>
2024-07-03 18:35:04 +0800

    Create countdown_timer.py

---
+++ MyCode/countdown_timer.py
@@ -0,0 +1,13 @@
+# countdown_timer.py
+import time
+
+def countdown(seconds):
+    while seconds:
+        mins, secs = divmod(seconds, 60)
+        timer = f'{mins:02d}:{secs:02d}'
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+countdown(10)
