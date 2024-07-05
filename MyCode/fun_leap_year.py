910acd7a9662f0c53f83eadd0da5ad1730ef2067
tianhai08 <166911681+tianhai08@users.noreply.github.com>
2024-07-05 21:11:56 +0800

    Create fun_leap_year.py

---
+++ MyCode/fun_leap_year.py
@@ -0,0 +1,8 @@
+# fun_leap_year.py
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    return False
+
+year = 2024
+print(f"{year} is a leap year: {is_leap_year(year)}")
