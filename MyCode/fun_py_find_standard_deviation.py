982ce3e911ec301eb40ab1abde5bf5ffd760120e
tianhai08 <166911681+tianhai08@users.noreply.github.com>
2024-07-23 21:04:19 +0800

    Create fun_py_find_standard_deviation.py

---
+++ MyCode/fun_py_find_standard_deviation.py
@@ -0,0 +1,7 @@
+# fun_py_find_standard_deviation.py
+import statistics
+
+def fun_py_find_standard_deviation(lst):
+    return statistics.stdev(lst)
+
+print(fun_py_find_standard_deviation([5, 3, 9, 1, 4]))
