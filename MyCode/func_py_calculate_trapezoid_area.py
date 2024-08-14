d710a744744235ece2023a7f1bbae7dec8602e9a
tianhai08 <166911681+tianhai08@users.noreply.github.com>
2024-08-14 19:57:22 +0800

    Create func_py_calculate_trapezoid_area.py

---
+++ MyCode/func_py_calculate_trapezoid_area.py
@@ -0,0 +1,5 @@
+# func_py_calculate_trapezoid_area.py
+def func_py_calculate_trapezoid_area(base1, base2, height):
+    return ((base1 + base2) * height) / 2
+
+print(func_py_calculate_trapezoid_area(5, 7, 10))
